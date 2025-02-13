4da4e9ec50e404c93c4820703738a851dc22c5e1
Victor Idam <idamvictorx1@gmail.com>
2025-02-13 15:31:49 +0100

    made the post feature to work

--- components/social-feed.tsx
+++ components/social-feed.tsx
@@ -17,7 +17,7 @@ const socialPosts = [
     content:
       "I just started my NEW JOB at Smith Teaching Hospital 🏥 Very excited about this new opportunity. 🤗",
     image:
-      "https://res.cloudinary.com/dyp8gtllq/image/upload/v1737075756/cld-sample-3.jpg",
+      "https://res.cloudinary.com/dyp8gtllq/image/upload/v1737075756/cld-sample-2.jpg",
     timestamp: "3 hours ago",
     avatar: "https://github.com/shadcn.png",
   },
@@ -27,10 +27,10 @@ const socialPosts = [
     content:
       "We are offering a 5% discount on your next Thai massage😊 Visit my store below ⬇️ https://bridged.fellag.com/marketplace",
     image:
-      "https://res.cloudinary.com/dyp8gtllq/image/upload/v1737075756/cld-sample-3.jpg",
+      "https://res.cloudinary.com/dyp8gtllq/image/upload/v1737075756/cld-sample-4.jpg",
     timestamp: "4 hours ago",
     avatar: "https://github.com/shadcn.png",
-    sponsored: true,
+    sponsored: false,
   },
 ];
 
