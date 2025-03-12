30983d589d85dc07c05090efd37736e8e168b77f
Victor Idam <idamvictorx1@gmail.com>
2025-03-12 13:12:27 +0100

    changes the link groups wordpress link from http to https

--- app/groups/page.tsx
+++ app/groups/page.tsx
@@ -2,7 +2,7 @@ export default function page() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <iframe
-        src="http://test.bridgedimpact.com/groups"
+        src="https://test.bridgedimpact.com/groups"
         width="100%"
         height="100%"
         style={{ border: "none" }}
