e64935d15e41b939afbc75bd26dfd8e4bdab29b1
Victor Idam <idamvictorx1@gmail.com>
2025-03-12 13:24:27 +0100

    changed it back to bridged link

--- app/groups/page.tsx
+++ app/groups/page.tsx
@@ -2,8 +2,8 @@ export default function page() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <iframe
-        // src="https://test.bridgedimpact.com/groups"
-        src="https://www.wikipedia.org/"
+        src="https://test.bridgedimpact.com/groups"
+        // src="https://www.wikipedia.org/"
         width="100%"
         height="100%"
         style={{ border: "none" }}
