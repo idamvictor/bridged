ab70638743e77bbaa8295fe8be4703f0eb311bd6
Victor Idam <idamvictorx1@gmail.com>
2025-03-12 13:19:14 +0100

    changed the url to wikipedia to see if it will work

--- app/groups/page.tsx
+++ app/groups/page.tsx
@@ -2,7 +2,8 @@ export default function page() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <iframe
-        src="https://test.bridgedimpact.com/groups"
+        // src="https://test.bridgedimpact.com/groups"
+        src="https://www.wikipedia.org/"
         width="100%"
         height="100%"
         style={{ border: "none" }}
